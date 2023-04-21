6cb18fdfb427dffc89d7e19562dd589236c0a1e3
Iann Rodrigues <iannrst11@gmail.com>
2023-04-21 16:28:56 -0300

    feat: Path aliases e Vitest globals

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,6 +1,6 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id';
 import { Answer } from "../entities/answer";
 import { AnswersRepository } from "../repositories/answers-repository";
-import { UniqueEntityId } from './../../core/entities/unique-entity-id';
 
 interface AnswerQuestionUseCaseRequest {
   instructorId: string
